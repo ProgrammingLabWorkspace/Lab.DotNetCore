e1b4b48ccc0cb7aa0ab8cd563a59d8461382aa3b
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-12-14 14:42:54 -0300

    Cadastrando aluno

--- ASP.NET Core Expert/ASP.NET MVC/MVC.FormLab/Controllers/AlunoController.cs
+++ ASP.NET Core Expert/ASP.NET MVC/MVC.FormLab/Controllers/AlunoController.cs
@@ -5,22 +5,33 @@ namespace MVC.FormLab.Controllers
 {
     public class AlunoController : Controller
     {
+        private AppDbContext DbContext;
+
+        public AlunoController(AppDbContext context)
+        {
+            DbContext = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult Criar()
         {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Criar([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao")]Aluno aluno)
+        public async Task<IActionResult> Criar([Bind("Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao")]Aluno aluno)
         {
             //TODO: Configurar Entity Framework
+
+            DbContext.Alunos.Add(aluno);
+            await DbContext.SaveChangesAsync();
+
             return View();
         }
     }
